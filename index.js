e31f03b134df96e6a738b626173fef9c220d9ef1
prashantyadav1397 <prashantyadav1397@gmail.com>
2021-09-02 12:33:05 +0530

    fs and http modules

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // Files
-
 // Synchronous blocking code
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log("Sync reading: ", textIn, "\n");
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log("Sync reading: ", textIn, "\n");
 
-const textOut = `This is an example of writing file to existing file: ${textIn}. \n Created on ${Date.now()}`;
-fs.writeFileSync("./txt/Output.txt", textOut);
+// const textOut = `This is an example of writing file to existing file: ${textIn}. \n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/Output.txt", textOut);
 
 // Asynchronous non-blocking code
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) {
-    console.log("Error!!! 👎👎");
-    return;
-  }
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log("async reading: ", data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log("async reading: ", data3);
-      fs.writeFile(
-        "./txt/final.txt",
-        `${data2} \n ${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("async writing completed ✔");
-        }
-      );
-    });
-  });
-});
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) {
+//     console.log("Error!!! 👎👎");
+//     return;
+//   }
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log("async reading: ", data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log("async reading: ", data3);
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2} \n ${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("async writing completed ✔");
+//         }
+//       );
+//     });
+//   });
+// });
 
-// HTTP
-
+// HTTP and URL
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is overview");
+  } else if (pathName === "/product") {
+    res.end("This is product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h2> Page Not found</h2>");
+  }
+
   res.end("Hello from the server!");
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(8000, "127.0.0.1", (err) => {
+  if (err) {
+    console.log(err);
+  }
   console.log("Listening to the incoming request...");
 });
